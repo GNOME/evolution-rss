f8adfddc5098fe3b96fbebd483189c7c383042da
Lucian Langa <lucilanga@src.gnome.org>
2007-12-12 11:14:45 +0000

    cleanups
    svn path=/trunk/; revision=93

--- src/rss.c
+++ src/rss.c
@@ -3865,7 +3865,7 @@ org_gnome_cooly_rss_refresh(void *ep, EMPopupTargetSelect *t)
 #if GTK_2_6
 	gtk_label_set_ellipsize (GTK_LABEL (label2), PANGO_ELLIPSIZE_START);
 #endif
-	gtk_label_set_justify(label2, GTK_JUSTIFY_CENTER);
+	gtk_label_set_justify(GTK_LABEL(label2), GTK_JUSTIFY_CENTER);
         readrss_label = gtk_label_new(_("Please wait"));
         if (!rf->progress_dialog)
         {
