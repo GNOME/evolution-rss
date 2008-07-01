ec9f308a54e63b36d2ceaa875aae55a9f873a3d6
Lucian Langa <lucilanga@src.gnome.org>
2008-07-01 11:49:53 +0000

    remove tag
    svn path=/tags/EVOLUTION-RSS_0_1_0; revision=323

--- server.mk
+++
@@ -1,9 +0,0 @@
-%.server.in: %.server.in.in
-	sed -e 's|\@PLUGIN_INSTALL_DIR\@|$(PLUGIN_INSTALL_DIR)|'	\
-	-e 's|\@ICON_DIR\@|$(ICON_DIR)|'	\
-	-e 's|\@VERSION\@|$(EVOLUTION_EXEC_VERSION)|' 			\
-	-e 's|\@EXEEXT\@|$(EXEEXT)|'				\
-	-e 's|\@SOEXT\@|$(SOEXT)|' $< > $@
-
-%_$(EVOLUTION_EXEC_VERSION).server: %.server
-	mv $< $@
