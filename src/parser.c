1910ba2732cdf7e74b49eecbad63b81e074fae8d
Lucian Langa <lucilanga@gnome.org>
2009-11-06 11:21:55 +0200

    don't strip empty fields

--- src/parser.c
+++ src/parser.c
@@ -1,16 +1,16 @@
 /*  Evoution RSS Reader Plugin
- *  Copyright (C) 2007-2008 Lucian Langa <cooly@gnome.eu.org> 
- *  
+ *  Copyright (C) 2007-2008 Lucian Langa <cooly@gnome.eu.org>
+ *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or 
+ *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
- *  
+ *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- *  
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -898,7 +898,7 @@ parse_channel_line(xmlNode *top, gchar *feed_name, char *main_date)
 					layer_find (top, "creator", NULL)));	//this catches dc:creator too. wrong!
 			}
 			//we might end with a subject containing nothing but spaces
-			g_strstrip (q);
+			if (q) g_strstrip (q);
 
 			/* empty creator? is this even legal
 			 * search for the source then
