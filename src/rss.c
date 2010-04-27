a357f5fd9c217617312ef5084a1b16551d074476
Lucian Langa <lucilanga@gnome.org>
2010-04-27 22:25:16 +0300

    use correct evo version in conditionals

--- src/rss.c
+++ src/rss.c
@@ -2841,7 +2841,7 @@ pixdone:			g_free(url);
 				feed_dir, tmp_file, NULL);
 		g_free(tmp_file);
 		g_free(feed_dir);
-#if EVOLUTION_VERSION >= 23100
+#if EVOLUTION_VERSION >= 23001
 		feed_file = g_filename_to_uri(tmp_path, NULL, NULL);
 #else
 		feed_file = g_strdup(tmp_path);
@@ -2873,7 +2873,7 @@ pixdone:			g_free(url);
 		tmp_file = g_build_filename (EVOLUTION_ICONDIR,
 				"rss-16.png",
 				NULL);
-#if EVOLUTION_VERSION >= 23100
+#if EVOLUTION_VERSION >= 23001
 		iconfile = g_filename_to_uri(tmp_file, NULL, NULL);
 #else
 		iconfile = g_strdup(tmp_file);
