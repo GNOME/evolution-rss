d52ec28c9ce51f251d80ce2038840146ca57478c
Lucian Langa <lucilanga@gnome.org>
2011-12-19 22:45:00 +0100

    update gen script to use xz as compression

--- scripts/create-snapshot.sh
+++ scripts/create-snapshot.sh
@@ -6,5 +6,5 @@ git clone git://git.gnome.org/evolution-rss /tmp/evolution-rss
 cd /tmp/evolution-rss
 git_version=`git log --oneline -1|cut -d" " -f1`
 gitdate=`date +%Y%m%d`
-git archive --format=tar --prefix=evolution-rss-${MAJOR_VERSION}/ ${git_version} | gzip > /tmp/evolution-rss-${MAJOR_VERSION}-${gitdate}.tar.gz
+git archive --format=tar --prefix=evolution-rss-${MAJOR_VERSION}/ ${git_version} | xz > /tmp/evolution-rss-${MAJOR_VERSION}-${gitdate}.tar.xz
 echo "HEAD: "`git log -1 --pretty=format:%h`
