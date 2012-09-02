74a26947691452fd61c2c160e6cef32328d2e218
Lucian Langa <lucilanga@gnome.org>
2012-09-02 23:22:03 +0200

    fix conversion feed

--- scripts/migrate-gconf-gsettings.sh
+++ scripts/migrate-gconf-gsettings.sh
@@ -3,20 +3,19 @@
 a=""
 IFS=
 XMLFEEDS=`gconftool-2 -g /apps/evolution/evolution-rss/feeds`
+if [ $XMLFEEDS == 'false' ]; then
+	echo "Feeds already converted. Nothing to do!"
+	exit 0
+fi
 for i in $XMLFEEDS; do
 if [ -z $a ]; then
 	a=$a$i
 else
 	a=$a"@"$i
 fi
-#a=$a"'"`echo $i|sed -e "s;\[;;g" -e "s;\];;g"`"';"
-#|grep -o -e "<url>.*</url>"|sed -e "s;<url>;;" -e "s;</url>;;"
 done
-#echo $a
-#exit
 IFS=$'\n'
-#b='['$a']'
 b=`echo $a|sed -e "s!\@!,!g"`
-echo $b
-#exit
+echo "gconf->dconf feeds migration done."
+gconftool-2 -s /apps/evolution/evolution-rss/feeds --type bool 0
 dconf write /org/gnome/evolution/plugin/rss/feeds \"$a\"
